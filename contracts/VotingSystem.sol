4e7aa781a5fe16e8994fe54f16b163ed0322f526
Vincent Guénin <guenin.vincent4@gmail.com>
2020-01-25 10:58:38 +0100

    Changed ballot implementation

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -28,13 +28,12 @@ contract VotingSystem {
         bytes32[] candidatesName;
     }
 
-    Ballot[] public ballots;
-    mapping(bytes32 => BallotExistence) ballotMapping;
+    bytes32[] ballots;
+    mapping(bytes32 => Ballot) ballotMapping;
     mapping(bytes32 => mapping(bytes32 => Candidate)) candidatesMapping;
 
     modifier ballotExists(bytes32 _ballotName) {
-        BallotExistence memory existence = ballotMapping[_ballotName];
-        require(existence.exists == true);
+        require(getBallotByName(_ballotName).state > State.NOT_CREATED);
         _;
     }
 
@@ -66,8 +65,8 @@ contract VotingSystem {
             candidatesName : new bytes32[](0)
             });
 
-        uint _ballotId = ballots.push(_newBallot) - 1;
-        ballotMapping[_ballotName] = BallotExistence({id : _ballotId, exists : true});
+        ballots.push(_ballotName);
+        ballotMapping[_ballotName] = _newBallot;
     }
 
     function openBallotVotes(bytes32 _ballotName) public ballotExists(_ballotName) ownsBallot(_ballotName) isState(_ballotName, State.CREATED) {
@@ -79,37 +78,27 @@ contract VotingSystem {
     }
 
     function getBallotByName(bytes32 _ballotName) internal view returns (Ballot memory) {
-        uint _ballotId = ballotMapping[_ballotName].id;
-        return ballots[_ballotId];
+        return ballotMapping[_ballotName];
     }
 
     function getBallot(bytes32 _ballotName) view public ballotExists(_ballotName) returns (Ballot memory) {
-        uint ballotId = ballotMapping[_ballotName].id;
-        return ballots[ballotId];
+        return getBallotByName(_ballotName);
     }
 
     function getStorageBallot(bytes32 _ballotName) view internal ballotExists(_ballotName) returns (Ballot storage) {
-        uint ballotId = ballotMapping[_ballotName].id;
-        return ballots[ballotId];
-    }
-
-    function getBallotId(bytes32 _ballotName) view public returns (uint) {
-        return ballotMapping[_ballotName].id;
+        return ballotMapping[_ballotName];
     }
 
     function isCreated(bytes32 _ballotName) view public ballotExists(_ballotName) returns (bool) {
-        uint ballotId = ballotMapping[_ballotName].id;
-        return ballots[ballotId].state == State.CREATED;
+        return getBallotByName(_ballotName).state == State.CREATED;
     }
 
     function isOpened(bytes32 _ballotName) view public ballotExists(_ballotName) returns (bool) {
-        Ballot memory _ballot = getBallot(_ballotName);
-        return _ballot.state == State.OPENED;
+        return getBallotByName(_ballotName).state == State.OPENED;
     }
 
     function isEnclosed(bytes32 _ballotName) view public ballotExists(_ballotName) returns (bool) {
-        Ballot memory _ballot = getBallot(_ballotName);
-        return _ballot.state == State.ENCLOSED;
+        return getBallotByName(_ballotName).state == State.ENCLOSED;
     }
 
     function isBallotCandidate(bytes32 _ballotName, bytes32 _candidateName) internal view returns (bool) {
